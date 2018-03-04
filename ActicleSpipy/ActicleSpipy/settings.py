96a91f14e71b5359eecde66b8816cf80b38928cd
beizhongshashui <13717546725@163.com>
2018-03-04 23:48:28 +0800

    完成jobbole上的文章题目，点赞，评论数的抓取

--- ActicleSpipy/ActicleSpipy/settings.py
+++ ActicleSpipy/ActicleSpipy/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'ActicleSpipy.spiders'
 #USER_AGENT = 'ActicleSpipy (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- ActicleSpipy/ActicleSpipy/spiders/jobbole.py
+++ ActicleSpipy/ActicleSpipy/spiders/jobbole.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http.response import Request
 
 
 class JobboleSpider(scrapy.Spider):
-    name = 'jobbole'
-    allowed_domains = ['bolg.jobbole.com']
-    start_urls = ['http://bolg.jobbole.com/']
+    name = "jobbole"
+    allowed_domains = ["http://blog.jobbole.com/"]
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        pass
+    #抓取所有文章的题目，评论数，收藏数和点赞数
+
+        # 一页的文章连接数
+        article_link_list =response.css('#archive .floated-thumb a.archive-title::attr(href)').extract()
+        next_page_link=response.css('.next.page-numbers::attr(href)').extract()
+
+        for url in article_link_list:
+
+            yield Request(url=url, callback=self.parse_detail)
+
+        meta ={
+            'data':'parse_data'
+        }
+
+        # 进行翻页功能
+        if next_page_link:
+            yield Request(url=next_page_link,callback=self.parse,meta=meta)
+
+
+    def parse_detail(self,response):
+        article_title = response.css('div.entry-header h1::text').extract()[0]
+        # 文章的点赞数
+        praise_num = response.css('.post-adds span h10::text').extract()[0]
+
+
+        data = response.meta['data']
+
+
+
+
